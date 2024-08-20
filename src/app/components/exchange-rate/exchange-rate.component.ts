07fc4599d0a6808fb1c8d79df02aca495bd0b009
Valeria <perkayay@gmail.com>
2024-08-20 11:23:27 +0300

    feat: create exchange rate component

---
+++ src/app/components/exchange-rate/exchange-rate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exchange-rate',
+  standalone: true,
+  imports: [],
+  templateUrl: './exchange-rate.component.html',
+  styleUrl: './exchange-rate.component.scss'
+})
+export class ExchangeRateComponent {
+
+}
